c0473356c2ad205176b5e9b310e72cbf8c416864
RichardKim <hiddeneventor@gmail.com>
2021-04-24 23:53:57 +0900

    초기 DB 세팅 및 Entity 설계

---
+++ src/main/java/com/richard/book/domain/Book.java
@@ -0,0 +1,17 @@
+package com.richard.book.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String author;
+}
