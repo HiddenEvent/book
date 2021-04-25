772cac715f4f0647f6db6b8bef6541a567e1c2d2
RichardKim <hiddeneventor@gmail.com>
2021-04-25 12:21:45 +0900

    Service, Repository 설계

--- src/main/java/com/richard/book/domain/Book.java
+++ src/main/java/com/richard/book/domain/Book.java
@@ -1,10 +1,17 @@
 package com.richard.book.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Book {
 
