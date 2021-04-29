45a0e7d737c5e16435496834e61a7ea959c0e0ac
RichardKim <hiddeneventor@gmail.com>
2021-04-30 00:07:58 +0900

    BookService - 유닛테스트 BookRepository - 유닛테스트

--- src/test/java/com/richard/book/domain/BookRepositoryUnitTest.java
+++ src/test/java/com/richard/book/domain/BookRepositoryUnitTest.java
@@ -3,11 +3,14 @@ package com.richard.book.domain;
 
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // 단위 테스트 (DB 관련된 Bean이 IOC에 등록되면 됨)
 
@@ -19,6 +22,18 @@ public class BookRepositoryUnitTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @Test
+    public void save_테스트(){
+        // given
+        Book book = new Book(null,"책제목1","책저자1");
+
+        // when
+        Book bookEntity = bookRepository.save(book);
+
+        // then
+        assertEquals("책제목1", bookEntity.getTitle());
+
+    }
 
 
 }
--- src/test/java/com/richard/book/service/BookServiceUnitTest.java
+++ src/test/java/com/richard/book/service/BookServiceUnitTest.java
@@ -1,9 +1,14 @@
 package com.richard.book.service;
+import com.richard.book.domain.Book;
 import com.richard.book.domain.BookRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  단위 테스트( Service와 관련된 애들만 메모리에 띄우면 됨.)
@@ -18,5 +23,24 @@ public class BookServiceUnitTest {
 
     @Mock
     private BookRepository bookRepository;
+
+    @Test
+    public void 저장하기_테스트() {
+        // BODMocikto 방식
+        // given
+        Book book = new Book();
+        book.setTitle("책제목1");
+        book.setAuthor("책저자1");
+
+        // stub - 동작 지정
+        when(bookRepository.save(book)).thenReturn(book);
+
+        // test execute
+        Book bookEnitty = bookService.저장하기(book);
+
+        assertEquals(bookEnitty, book);
+
+
+    }
 
 }
